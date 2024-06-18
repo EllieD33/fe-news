3e64a6c1d1e130fa40bc8e288936343f5dc24911
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-18 11:48:15 +0100

    adds navigationlinks to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { useColorMode, IconButton, Heading, Flex } from "@chakra-ui/react";
+import { useColorMode, IconButton, Heading, Flex, Link as ChakraLink, Text } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from 'react-router-dom'
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 const Header = () => {
@@ -6,13 +7,22 @@ const Header = () => {
     return (
         <header>
             <Flex justify="space-between">
-                <Heading as="h1">NewsHub</Heading>
+                <Heading  as="h1">NewsHub</Heading>
                 <IconButton
                         ml={2}
                         onClick={toggleColorMode}
                         aria-label="Toggle light/dark mode"
                         icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
                     />
+            </Flex>
+            <Flex justify="center" mb={4} >
+                <ChakraLink as={ReactRouterLink} to="/home" >
+                    <Text>Home</Text>
+                </ChakraLink>
+                <Text mx={4} >|</Text>
+                <ChakraLink as={ReactRouterLink} to="/stories">
+                    <Text>All stories</Text>
+                </ChakraLink>
             </Flex>
         </header>
     )
