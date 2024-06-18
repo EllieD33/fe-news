975361bede74d02a1a70b62cfe9c214507e0de37
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-18 12:20:51 +0100

    conditionally renders delete button if loggedInUser is comment author

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,12 +1,20 @@
-import { Card, Heading, Text } from "@chakra-ui/react"
+import { Card, Heading, IconButton, Text, Flex } from "@chakra-ui/react"
+import { UserContext } from "../contexts/UserContext";
+import { useContext } from "react";
 import { formatDate, formatTime } from "../utils/helpers"
+import { DeleteIcon } from "@chakra-ui/icons";
 
 const CommentCard = ({ comment }) => {
+    const { loggedInUser } = useContext(UserContext)
+
     return (
         <Card tabIndex={0} as="article" m={2} p={2} minW="300px" maxW="480px" width={{ base: "100%", sm: "350px", md: "480px" }} >
             <Heading as="h4" fontSize="md" >{comment.author} on {formatDate(comment.created_at)} at {formatTime(comment.created_at)}</Heading>
             <Text my={2} >{comment.body}</Text>
-            <Text>Votes: {comment.votes}</Text>
+            <Flex justify="space-between" align="center" >
+                <Text>Votes: {comment.votes}</Text>
+                {loggedInUser.username === comment.author ? <IconButton aria-label="Delete comment" icon={<DeleteIcon size="24px" />} w="20px" colorScheme="teal" size="sm" /> : null}
+            </Flex>
         </Card>
     )
 }
