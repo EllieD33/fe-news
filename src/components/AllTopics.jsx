7ddd4c425aa804e08f976896adabdc766060bc7b
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-19 11:22:10 +0100

    fetches and displays data for topic cards

--- src/components/AllTopics.jsx
+++ src/components/AllTopics.jsx
@@ -1,12 +1,26 @@
-import { Flex } from "@chakra-ui/react"
+
+import { Flex, Heading } from "@chakra-ui/react"
 import TopicCard from "./TopicCard"
+import { fetchAllTopics } from "../utils/api"
+import { useEffect, useState } from "react"
 
 const AllTopics = () => {
+    const [topics, setTopics] = useState([]);
+
+    useEffect(() => {
+        fetchAllTopics().then(({ topics }) => {
+            setTopics(topics)
+        })
+    }, [])
+
     return (
-        <Flex h="80vh" as="main" justify="center" align="center" direction="column" >
-            <TopicCard/>
-            <TopicCard/>
-            <TopicCard/>
+        <Flex mt={10} as="main" justify="center" align="center" direction="column" >
+            <Heading>All topics</Heading>
+            {
+                topics.map((topic) => (
+                    <TopicCard topic={topic.slug} description={topic.description} />
+                ))
+            }
         </Flex>
     )
 }
--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,11 +1,12 @@
 import { Flex, Heading, Text, Card } from "@chakra-ui/react"
+import { capitaliseFirstLetter } from "../utils/helpers"
 
-const TopicCard = () => {
+const TopicCard = ({ topic, description }) => {
     return (
         <Card my={4} w="80%" as="section" transition="all 0.2s" _hover={{ transform: "translateY(-4px)", shadow: "lg" }}>
             <Flex p={4} direction="column" justify="center" align="center" >
-                <Heading as="h3">Topic name</Heading>
-                <Text>Number of stories</Text>
+                <Heading as="h3">{topic && capitaliseFirstLetter(topic)}</Heading>
+                <Text>{description && description}</Text>
             </Flex>
         </Card>
     )
