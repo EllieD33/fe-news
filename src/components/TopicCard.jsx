b069ae37bc52c80ec625ddffd8c98df01a0399c9
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-19 11:44:29 +0100

    adds routing to topic cards

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,14 +1,19 @@
-import { Flex, Heading, Text, Card } from "@chakra-ui/react"
+import { Flex, Heading, Text, Card, LinkBox, LinkOverlay } from "@chakra-ui/react"
+import { Link as ReactRouterLink } from "react-router-dom"
 import { capitaliseFirstLetter } from "../utils/helpers"
 
 const TopicCard = ({ topic, description }) => {
     return (
-        <Card my={4} w="80%" as="section" transition="all 0.2s" _hover={{ transform: "translateY(-4px)", shadow: "lg" }}>
-            <Flex p={4} direction="column" justify="center" align="center" >
-                <Heading as="h3">{topic && capitaliseFirstLetter(topic)}</Heading>
-                <Text>{description && description}</Text>
-            </Flex>
-        </Card>
+        <LinkBox>
+            <Card my={4} w="80%" minW="420px" as="section" transition="all 0.2s" _hover={{ transform: "translateY(-4px)", shadow: "lg", color: "teal" }}>
+                <LinkOverlay as={ReactRouterLink} to={`/topics/${topic}`} >
+                    <Flex p={4} direction="column" justify="center" align="center" >
+                        <Heading as="h3">{topic && capitaliseFirstLetter(topic)}</Heading>
+                        <Text>{description && description}</Text>
+                    </Flex>
+                </LinkOverlay>
+            </Card>
+        </LinkBox>
     )
 }
 
