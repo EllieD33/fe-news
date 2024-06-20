454ceec42a82541e89f64ef0de4780ffe478bf5a
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-20 09:14:13 +0100

    adds conditional rendering to PageNotFound based on pathname

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,14 +1,26 @@
 import { Heading, Text, Flex, Box, Image, Link as ChakraLink } from "@chakra-ui/react"
-import { Link as ReactRouterLink } from 'react-router-dom'
+import { Link as ReactRouterLink, useLocation } from 'react-router-dom'
 
 
 const PageNotFound = () => {
+    const location = useLocation();
+    const pathname = location.pathname;
+
+    let conditionalAddTopic = null;
+    if (pathname.startsWith("/topics/")) {
+        conditionalAddTopic = (
+            <Text mt={4}>
+                This topic doesn't exist - do you want to add it?
+            </Text>
+        );
+    }
+
     return (
-        <Flex as="main" justify="center" align="center" direction="column" h="80%" >
+        <Flex mt={10} as="main" justify="center" align="center" direction="column" h="80%" >
             <Heading mb={4} >Nothing to see here!</Heading>
-            <Box w="60%" pb="56.25%"  pos="relative" overflow="hidden" borderRadius="md"  boxShadow="md" >
-                <Image src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGs2NzYxazBybzBnM3psZmlsY2p1NHZoZHNqdmc1YTN6OW0yMXdvOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0RAvDxfdksWy39YG4T/giphy.webp" alt="Page not found" objectFit="cover" width="100%" height="100%" style={{ position: 'absolute' }}/>
-            </Box>
+                <Image src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGs2NzYxazBybzBnM3psZmlsY2p1NHZoZHNqdmc1YTN6OW0yMXdvOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0RAvDxfdksWy39YG4T/giphy.webp" alt="Page not found" objectFit="cover" width="100%" height="100%" maxW="350px" maxH="350px" borderRadius="5px" />
+            
+            {conditionalAddTopic}
             <ChakraLink as={ReactRouterLink} to="/home" mt={4} >Back to Home</ChakraLink>
         </Flex>
     )
