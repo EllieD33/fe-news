39b49d38bd1d57a28f2d4abeb663136ab5b28514
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-06-22 12:08:15 +0100

    adds isVaoting state to vote form to prevent spam clicks giving mutliple votes

--- src/components/forms/VoteForm.jsx
+++ src/components/forms/VoteForm.jsx
@@ -7,10 +7,12 @@ import { updateArticleVotes } from "../../utils/api";
 const VoteForm = ({ story, setStory }) => {
     const { loggedInUser } = useContext(UserContext);
     const [hasVoted, setHasVoted] = useState(false);
+    const [isVoting, setIsVoting] = useState(false); 
     const [commentCount, setCommentCount] = useState(story.comment_count);
 
     const handleVote = (event) => {
         event.preventDefault();
+        setIsVoting(true);
 
         if (hasVoted) {
             alert('You have already voted on this story.');
@@ -31,21 +33,22 @@ const VoteForm = ({ story, setStory }) => {
             const { article } = updatedStory;
             setStory({...article, comment_count: commentCount});
             setHasVoted(true);
+            setIsVoting(false);
         }).catch((error) => {
-            console.error('Error updating story votes:', error);
             setStory(prevStory => ({
                 ...prevStory,
                 votes: originalVotes
             }));
+            setIsVoting(false);
             alert('Failed to update votes. Please try again.')
         });
     }
 
     return (
         <Flex as="form" align="center">
-            <IconButton type="submit" onClick={handleVote} isDisabled={hasVoted} name="downVote" border="none" tabIndex={0} mr={1} icon={<TiArrowDownOutline size="24px" />} aria-label='down vote article' variant="outline" colorScheme="teal" size="sm" />
+            <IconButton type="submit" onClick={handleVote} isDisabled={hasVoted || isVoting} name="downVote" border="none" tabIndex={0} mr={1} icon={<TiArrowDownOutline size="24px" />} aria-label='down vote article' variant="outline" colorScheme="teal" size="sm" />
             <Text>{`${story.votes}`}</Text>
-            <IconButton type="submit" onClick={handleVote} isDisabled={hasVoted} name="upVote" border="none" tabIndex={0} ml={1} icon={<TiArrowUpOutline size="24px" />} aria-label='up vote article' variant="outline" colorScheme="teal" size="sm" />
+            <IconButton type="submit" onClick={handleVote} isDisabled={hasVoted || isVoting} name="upVote" border="none" tabIndex={0} ml={1} icon={<TiArrowUpOutline size="24px" />} aria-label='up vote article' variant="outline" colorScheme="teal" size="sm" />
         </Flex>
     )
 }
