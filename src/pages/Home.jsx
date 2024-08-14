c346039ca7d2a5cf78469e4b28191730815c9b24
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-14 13:42:11 +0100

    adds loading error text to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ import FeaturedStories from "../components/containers/FeaturedStories";
 const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [recentStories, setRecentStories] = useState([]);
-    const [mostPopularStories, setMostPopularStories] = useState([])
+    const [mostPopularStories, setMostPopularStories] = useState([]);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         const recentStoriesPromise = fetchAllArticles(null, "created_at", "DESC", 3).then(({ articles }) => articles);
@@ -17,9 +18,11 @@ const Home = () => {
             .then(([recentArticles, popularArticles]) => {
                 setRecentStories(recentArticles);
                 setMostPopularStories(popularArticles);
+                setError("");
             })
             .catch(error => {
                 console.error('Error fetching articles:', error);
+                setError('There was an issue fetching the articles. Please refresh or try again later.')
             })
             .finally(() => {
                 setIsLoading(false);
@@ -35,11 +38,14 @@ const Home = () => {
                     <Text mx={2} textAlign="center">Enjoy your time here, and remember: It's nice to be nice!!!</Text>
                 </section>
                 {isLoading ? <Spinner /> :
-                <Flex wrap="wrap" align="flex-start" justify="center">
-                    <FeaturedStories title="What's new" subtitle="Recently added stories" stories={recentStories} />
-                    <FeaturedStories title="Trending" subtitle="Most popular stories" stories={mostPopularStories} />
-                </Flex>
-                }
+                error ? (
+                    <Text my={50} fontSize="xl" textAlign="center" >{error}</Text>
+                ) : (
+                    <Flex wrap="wrap" align="flex-start" justify="center">
+                        <FeaturedStories title="What's new" subtitle="Recently added stories" stories={recentStories} />
+                        <FeaturedStories title="Trending" subtitle="Most popular stories" stories={mostPopularStories} />
+                    </Flex>
+                )}
             </Flex>
         </>
     )
